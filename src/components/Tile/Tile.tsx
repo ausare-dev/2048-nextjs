0cdda3da5204957e4a8a9169ed25135b279861da
Ausare <asain_baitasov@mail.ru>
2024-09-01 21:11:38 +0600

    add styles for tiles

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -3,12 +3,20 @@ import styles from './Tile.module.scss'
 
 interface TileProps {
 	tile: number | null;
+	
 }
 
-const Tile: React.FC<TileProps> = ({tile }) => {
+const Tile: React.FC<TileProps> = ({tile}) => {
+	const getGifForTile = (value: number | null): string => {
+		if (value === null) {
+			return '';
+		}
+		return `/images/${value}.gif`;
+	};
+
 	return (
-		<div  className={styles.tile}>
-			{tile}
+		<div className={styles.tile}>
+				<img className={styles.img} src={getGifForTile(tile)} alt={`Tile ${tile}`} />
 		</div>
 	);
 };
